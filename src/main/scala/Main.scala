e534a65dd3682db098bdcec0812c8bf3e64381e4
Patrick Singer <sinpat224@hotmail.com>
2022-07-31 15:07:51 +0200

    host simple html

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,21 @@
 import cats.effect._
 import org.http4s._
+import org.http4s.headers.`Content-Type`
 import org.http4s.dsl.io._
 import cats.effect.unsafe.IORuntime
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.Router
 import com.comcast.ip4s.{ipv4, port}
+import web.HTML._
 
 object Main extends IOApp {
 
   val helloWorldService = HttpRoutes
     .of[IO] { case GET -> Root / "hello" / name =>
-      Ok(s"Hello, $name.")
+      Ok(
+        htmlRoot("", s"Hello $name"),
+        `Content-Type`.apply(MediaType.text.html)
+      )
     }
     .orNotFound
 
